f241c0a08e816fccfc67a1ff520997be511a3cbd
oemer_aran@live.de <oemer_aran@live.de>
2023-03-27 09:36:57 +0200

    test deploy faker remove

--- components/Expensive.vue
+++ components/Expensive.vue
@@ -6,19 +6,29 @@
   </div>
 </template>
 <script setup lang="ts">
-import { faker } from "@faker-js/faker";
+//import { faker } from "@faker-js/faker";
 
+// function createRandomUser() {
+//   return {
+//     id: faker.datatype.uuid(),
+//     birthday: faker.date.birthdate(),
+//     email: faker.internet.email(),
+//     firstName: faker.name.firstName(),
+//     lastName: faker.name.lastName(),
+//     sex: faker.name.sexType(),
+//     subscriptionTier: faker.helpers.arrayElement(["free", "basic", "business"]),
+//   };
+// }
 function createRandomUser() {
   return {
-    id: faker.datatype.uuid(),
-    birthday: faker.date.birthdate(),
-    email: faker.internet.email(),
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    sex: faker.name.sexType(),
-    subscriptionTier: faker.helpers.arrayElement(["free", "basic", "business"]),
+    id: "23423-235-2-235-23-2",
+    birthday: "15.05.23",
+    email: "hans@peter.de",
+    firstName: "hans",
+    lastName: "peter",
+    sex: "male",
+    subscriptionTier: "free",
   };
 }
-
-const users = Array.from({ length: 5 }).map(() => createRandomUser());
+const users = Array.from({ length: 50000 }).map(() => createRandomUser());
 </script>
