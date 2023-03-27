9988bb435973401b15c126003dd9cbc48ed3c68b
oemer_aran@live.de <oemer_aran@live.de>
2023-03-27 14:20:11 +0200

    test deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,25 @@
 export default defineNuxtConfig({
   nitro: {
     preset: "vercel-edge",
-    // prerender: {
-    //   crawlLinks: true,
-    //   routes: ["/prerender/1", "/prerender/fetch"],
-    // },
+    prerender: {
+      crawlLinks: true,
+      routes: ["/prerender/1", "/prerender/fetch"],
+    },
   },
   routeRules: {
-    // "/csr/**": { ssr: false },
-    // "/static/**": { static: true }, // Doesn't work at all
-
-    "/swr/**": { swr: 1 },
-
-    // "/universal/**": {}, // default
-    // "/redirect/from": { redirect: { to: "redirect/to", statusCode: 302 } }, // Doesn't work: redirects to /redirect/redirect/to
-    // "/redirect-from": { redirect: { to: "redirect/to", statusCode: 302 } }, // works
-    // "/picsum/**": { proxy: { to: "https://picsum.photos/**" } }, // doesn't seem to work
+    "/csr/**": { ssr: false },
+    "/static/**": { static: true },
+    "/cache/**": {
+      cache: { maxAge: 1 },
+      headers: { "cache-control": "no-store" },
+    },
+    "/cache-swr/**": { cache: { maxAge: 1, swr: true } },
+    "/swr/**": { swr: 1 * 60 },
+    "/swr-long/**": { swr: 1 * 60 * 10 },
+    "/swr-short/**": { swr: 1 * 5 },
+    "/universal/**": {}, // default
+    "/redirect/from": { redirect: { to: "redirect/to", statusCode: 302 } }, // Doesn't work: redirects to /redirect/redirect/to
+    "/redirect-from": { redirect: { to: "redirect/to", statusCode: 302 } }, // works
+    "/picsum/**": { proxy: { to: "https://picsum.photos/**" } }, // doesn't seem to work
   },
 });
