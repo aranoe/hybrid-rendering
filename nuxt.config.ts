dd755be5ec89153d0b4e519caa2a9b2323b7ae39
oemer_aran@live.de <oemer_aran@live.de>
2023-03-27 12:46:23 +0200

    test deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,20 @@
 export default defineNuxtConfig({
   nitro: {
     preset: "vercel-edge",
-    prerender: {
-      crawlLinks: true,
-      routes: ["/prerender/1", "/prerender/fetch"],
-    },
+    // prerender: {
+    //   crawlLinks: true,
+    //   routes: ["/prerender/1", "/prerender/fetch"],
+    // },
   },
   routeRules: {
-    "/csr/**": { ssr: false },
-    "/static/**": { static: true }, // Doesn't work at all
+    // "/csr/**": { ssr: false },
+    // "/static/**": { static: true }, // Doesn't work at all
 
     "/swr/**": { swr: 5 },
 
-    "/universal/**": {}, // default
-    "/redirect/from": { redirect: { to: "redirect/to", statusCode: 302 } }, // Doesn't work: redirects to /redirect/redirect/to
-    "/redirect-from": { redirect: { to: "redirect/to", statusCode: 302 } }, // works
-    "/picsum/**": { proxy: { to: "https://picsum.photos/**" } }, // doesn't seem to work
+    // "/universal/**": {}, // default
+    // "/redirect/from": { redirect: { to: "redirect/to", statusCode: 302 } }, // Doesn't work: redirects to /redirect/redirect/to
+    // "/redirect-from": { redirect: { to: "redirect/to", statusCode: 302 } }, // works
+    // "/picsum/**": { proxy: { to: "https://picsum.photos/**" } }, // doesn't seem to work
   },
 });
